6383b6a4592fe5a5ecc76d706dcabb94b8f75e5e
Jalamang <Jalamangjanka@gmail.com>
2021-10-14 16:07:30 -0400

    Modified

--- scripts.js
+++ scripts.js
@@ -3,63 +3,50 @@ fetch("https://ghibliapi.herokuapp.com/films")
   .then((movies) => {
     //  console.log(movies);
 
-   
     const dropDown = document.querySelector("#pick-movie");
     movies.forEach((movie) => {
       const option = document.createElement("option");
       option.setAttribute("value", movie.title);
       option.textContent = movie.title;
       dropDown.append(option);
-     
     });
 
-const movieTitle = document.querySelector("#title");
-const yearRelease = document.querySelector("#year-release");
-const description = document.querySelector("#description");
+    const movieTitle = document.querySelector("#title");
+    const yearRelease = document.querySelector("#year-release");
+    const description = document.querySelector("#description");
 
-let selectedMovie;
+    let selectedMovie;
 
-dropDown.addEventListener("change", (/*even*/) => {
-  //event.target.value 
-  movies.forEach((movie) => {
-    if (dropDown.value === movie.title) {
-           selectedMovie =  movie
-        //   console.log(movies)
-      movieTitle.textContent = movie.title;
-      yearRelease.textContent = movie.release_date;
-      description.textContent = movie.description;
+    dropDown.addEventListener("change", (/*even*/) => {
+      //event.target.value
+      movies.forEach((movie) => {
+        if (dropDown.value === movie.title) {
+          selectedMovie = movie;
+          //   console.log(movies)
+          movieTitle.textContent = movie.title;
+          yearRelease.textContent = movie.release_date;
+          description.textContent = movie.description;
+        }
+      });
+    });
+
+    const movieUl = document.querySelector("#ulMovie");
+    const addButton = document.querySelector("#submit-form");
+    const input = document.querySelector("#input-form");
+
+    let newMovie;
+    input.addEventListener("input", (event) => {
+      event.preventDefault();
+        newMovie = document.createElement("li");
+        newMovie.textContent = `${selectedMovie.title}: ${input.value} `;
       
-    }
+    });
+
+    addButton.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      //if(!document.querySelector('ul').textContent.includes(input.value)){
+      movieUl.prepend(newMovie);
+      //}
+    });
   });
-})
-
-const movieUl = document.querySelector("#ulMovie");
-const addButton = document.querySelector("#submit-form");
-const input = document.querySelector("#input-form")
-
-let newMovie;
-input.addEventListener('input', (event)=>{
-
-    event.preventDefault()
-    movies.forEach((movie) => {  
-     console.log(movies)
-
-     newMovie = document.createElement("li");
-     newMovie.textContent = `<strong> ${movie.title}: ${input.value} </strong>`
-     const li = document.querySelector('li') 
-     li.setAttribute('style', 'font-weight:bold');
-   }); 
-   
-    })
-    
-    addButton.addEventListener("click", (event)=>{
-        event.preventDefault()
-     
-        if(!document.querySelector('ul').textContent.includes(input.value)){
-            movieUl.prepend(newMovie); 
-            }   
-            event.target.reset();
-     });  
-
-
-  })
