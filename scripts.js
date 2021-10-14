446e9f648011da84af98297f265633787503bcfe
Jalamang <Jalamangjanka@gmail.com>
2021-10-14 13:41:41 -0400

    added review section && display-info section

--- scripts.js
+++ scripts.js
@@ -12,4 +12,62 @@ fetch("https://ghibliapi.herokuapp.com/films")
       dropDown.append(option);
      
     });
+
+const movieTitle = document.querySelector("#title");
+const yearRelease = document.querySelector("#year-release");
+const description = document.querySelector("#description");
+
+let selectedMovie;
+
+dropDown.addEventListener("change", (/*even*/) => {
+  //event.target.value 
+  movies.forEach((movie) => {
+    if (dropDown.value === movie.title) {
+           selectedMovie =  movie
+          console.log(movies)
+      movieTitle.textContent = movie.title;
+      yearRelease.textContent = movie.release_date;
+      description.textContent = movie.description;
+      
+    }
+  });
 })
+
+const membersUl = document.querySelector("#ulMovie");
+const addButton = document.querySelector("#submit-form");
+const input = document.querySelector("#input-form")
+console.log(input.value)
+
+const teamMember = () => {
+    movies.forEach((movie) => {  
+        let newMovie = document.createElement("li");
+        console.log(document.querySelector("#input-form"))
+        newMovie.textContent = `${movie.title}; ${input.value} ` 
+       
+      });
+//     if(!document.querySelector('ul').textContent.includes(newMovie.textContent)){
+ membersUl.prepend(newMovie);
+// }
+
+addButton.addEventListener("click", teamMember);
+
+}
+
+// document.querySelector('#save-button').addEventListener('click',() =>{
+//     // if(textArea.textContent.length == 0){
+//     //   alert('You must enter')
+//     // }
+//     const CharacterName = document.createElement('li')
+//     CharacterName.textContent = textArea.value
+//     console.log(CharacterName.textContent +"<><><>")
+//     CharacterName.style.color = colorSelection.value
+//     CharacterName.fontFamily =  fontSelection.value
+//       if(!document.querySelector('#saved-list').textContent.includes(CharacterName.textContent)){
+//       document.querySelector('#saveText').append(CharacterName)
+//       }
+//   })
+
+
+
+
+  })
